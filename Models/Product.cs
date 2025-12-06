daad62af06a279fa321dfcb7166ce722ba5676f8
danillossampaio <87111468+danillossampaio@users.noreply.github.com>
2025-12-06 18:02:29 +0000

    Add files via upload

---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GymProjectApp.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Range(0.01, 10000)]
+        public decimal Price { get; set; }
+
+        public string Category { get; set; } = string.Empty;
+
+        [Range(0, int.MaxValue)]
+        public int Stock { get; set; }
+    }
+}
